24368d9ebeb127f69a57c1c9b89ae99424307c67
MathieuFoulon <matfoulon@gmail.com>
2023-01-28 16:37:20 +0100

    tests are ok, issue was in FareCalulatorService

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,11 +10,18 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        // starting with milliseconds, then seconds, minutes, hours ( a bit long but more understandable )
+        long inHour = ticket.getInTime().getTime();
+        long outHour = ticket.getOutTime().getTime();
+        long difOutAndInSec = ((outHour - inHour )/ 1000 );
+        float difOutAndInMin = (float)difOutAndInSec / 60;
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+
+        // duration is now in hour and is a float, that's why tests failed before ( couldn't proceed 0.5 hours )
+
+        // For free ticket, we can simply say that if vehicle stay less than 30 minutes, it's like it is 0 minute: so 0 * fare = 0
+        float duration = difOutAndInMin / 60 < 0.5 ? 0 : difOutAndInMin /60;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
@@ -25,7 +32,7 @@ public class FareCalculatorService {
                 ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default: throw new IllegalArgumentException("Unknown Parking Type");
         }
     }
 }
